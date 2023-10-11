ae3dcd6dd31574c3a1b3ac1d9f35228dad4f3851
Semen Alekseev <semyon492@ya.ru>
2023-10-11 15:47:21 +0300

    Upd

--- system/Mozg.php
+++ system/Mozg.php
@@ -27,27 +27,27 @@ class Mozg
             \session_id($_POST['PHPSESSID']);
         }
 
-//        $db = require ENGINE_DIR . '/data/db_config.php';
-//        Registry::set('db', $db);
+        //        $db = require ENGINE_DIR . '/data/db_config.php';
+        //        Registry::set('db', $db);
 
-//        $checkLang = I18n::getLang();
+        //        $checkLang = I18n::getLang();
         $lang = I18n::dictionary();
         Registry::set('lang', $lang);
 
-//        $config = settings_get();
+        //        $config = settings_get();
         Registry::set('server_time', \time());
         (new classes\Auth)->login();
-//        if ($config['offline'] === 'yes') {
-//            include ENGINE_DIR . '/modules/offline.php';
-//        }
+        //        if ($config['offline'] === 'yes') {
+        //            include ENGINE_DIR . '/modules/offline.php';
+        //        }
         /** @var array $user_info */
         $user_info = Registry::get('user_info');
-//        if ($user_info['user_delet'] > 0) {
-//            include_once ENGINE_DIR . '/modules/profile_delet.php';
-//        }
-//        if ((Registry::get('logged') && $user_info['user_ban_date'] >= Registry::get('server_time')) || (Registry::get('logged') && ($user_info['user_ban_date'] === '0'))) {
-//            include_once ENGINE_DIR . '/modules/profile_ban.php';
-//        }
+        //        if ($user_info['user_delet'] > 0) {
+        //            include_once ENGINE_DIR . '/modules/profile_delet.php';
+        //        }
+        //        if ((Registry::get('logged') && $user_info['user_ban_date'] >= Registry::get('server_time')) || (Registry::get('logged') && ($user_info['user_ban_date'] === '0'))) {
+        //            include_once ENGINE_DIR . '/modules/profile_ban.php';
+        //        }
 
         /**
          * Если юзер авторизован,
@@ -96,6 +96,11 @@ class Mozg
             '/api/account/restore' => 'Api@restore',
             '/api/account/reset_password' => 'Api@reset_password',
             '/api/account/change_pass' => 'Api@change_pass',
+            '/api/account/change_name' => 'Api@change_name',
+            '/api/account/change_avatar' => 'Api@change_avatar',
+            '/api/users/profile' => 'Profile@profile',
+            '/api/albums/all' => 'Albums@all',            
+            '/api/search' => 'Search@all',            
             
             '/api' => 'Api@main',
             '/api/profile' => 'Profile@api',
@@ -134,16 +139,7 @@ class Mozg
             '/balance/payment_2' => 'Balance@payment_2',
             '/balance/ok_payment' => 'Balance@ok_payment',
 
-            '/balance/payment' => 'Balance@createOrderBox',//1
-            '/pay/test/create/' => 'Balance@payCreateTest',//2
-            '/pay/fkw/create/' => 'Balance@payCreateFkw',//2
-            '/pay/test/' => 'Balance@payMain',//3
-            '/pay/test/success/' => 'Balance@payTestSuccess',//4
-            '/pay/fw/check/' => 'Balance@checkFWKassa',
-            '/pay/fw/success/' => 'Balance@main',//4
-            '/pay/fkw/success/' => 'Balance@main',//4
-            '/pay/fw/bad/' => 'Balance@main',//4
-            '/pay/fkw/bad/' => 'Balance@main',//4
+            '/balance/payment' => 'Balance@createOrderBox',
 
             '/support' => 'Support@main',
 
